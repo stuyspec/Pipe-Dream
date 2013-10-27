c6ff4af4a70bfaca72484a1e098f55da813599d7
Andrew Fischer <afischer15@mac.com>
2013-10-27 16:29:27 +0000

    Fixing join page to fit with spec

--- page-join.php
+++ page-join.php
@@ -3,15 +3,15 @@
 	<div id="page-join" class="content row">
 		<nav id="page-navigation">
 			<ul>
-				<li><a href="<? bloginfo('wpurl'); ?>/about/" title="About Pipe Dream">About</a></li>
-				<li><a href="<? bloginfo('wpurl'); ?>/advertise/" title="Advertise in Pipe Dream">Advertise</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/about/" title="About The Spectator">About</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/advertise/" title="Advertise in The Spectator">Advertise</a></li>
 				<!-- <li><a href="<? bloginfo('wpurl'); ?>/donate/" title="Donate to Pipe Dream">Donate</a></li> -->
-				<li class="active"><a href="<? bloginfo('wpurl'); ?>/join/" title="Join Pipe Dream">Join</a></li>
+				<li class="active"><a href="<? bloginfo('wpurl'); ?>/join/" title="Join The Spectator">Join</a></li>
 				<!-- <li><a href="<? bloginfo('wpurl'); ?>/staff/" title="Faces behind Pipe Dream">Staff</a></li> -->
-				<li><a href="<? bloginfo('wpurl'); ?>/contact/" title="Contact Pipe Dream">Contact</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/contact/" title="Contact The Spectator">Contact</a></li>
 			</ul>
 		</nav>
-		<h1 class="page-title">Join Pipe Dream</h1>
+		<h1 class="page-title">Join The Spectator</h1>
 		<section class="hero clearfix">
 			<figure class="hero-image">
 				<img src="<? bloginfo( 'template_url' ); ?>/img/pages/gim.png">
@@ -19,107 +19,122 @@
 			<p>The Spectator, founded in 1915, is Stuyvesant High School’s bi-weekly, student-run newspaper. It is independent of any other organization, and as “The Pulse of the Student Body,” is a forum for student expression. It is accessible to all parties and is available at no cost. We need your help to make this paper the best it can be. Ask any staffer and they will tell you: joining <i>The Spectator</i> was one of the best decisions of their college career.</p>
 			<!-- <a href="#" class="button">View Openings</a> -->
 		</section>
+		
 		<section class="page-section">
-			<h2 class="page-section-title">Sections<small>Click on a section to learn more</small></h2>
+			<h2 class="page-section-title">Departments <small>Click on a department to learn more</small></h2>
 
 			<!-- Writing Sections -->
 			<div data-section="news" class="section-description" style="display: block;">
 				<h3 class="section-title">News</h3>
 				<p>The news section is responsible for covering news and events for both Binghamton University and the greater Binghamton area. News reporters have the opportunity to interview community leaders and gain a deeper understanding of the University’s inner workings.</p>
-				<a href="mailto:news@bupipedream.com" class="section-email">news@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/news-application/" class="section-email">Apply to News</a>
 			</div>
 			<div data-section="sports" class="section-description">
 				<h3 class="section-title">Sports</h3>
 				<p>The sports section focuses on Bearcat coverage, writing game recaps and previews, features on players and coaches, and breaking news stories on the teams when the occasions arise. Sports writers occasionally comment on professional sporting news as well.</p>
-				<a href="mailto:sports@bupipedream.com" class="section-email">sports@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/sports-application/" class="section-email">Apply to Sports</a>
 			</div>
 			<div data-section="opinion" class="section-description">
-				<h3 class="section-title">Opinion</h3>
+				<h3 class="section-title">Opinions</h3>
 				<p>The opinion section publishes student columns and staff editorials that offer thought-provoking commentary on topics relevant to the campus community. While some columns are free form, other authors choose a theme, such as humor or politics, to focus on from issue to issue.</p>
-				<a href="mailto:opinion@bupipedream.com" class="section-email">opinion@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/opinions-application/" class="section-email">Apply to Opinions</a>
 			</div>
-			<div data-section="release" class="section-description">
-				<h3 class="section-title">Release</h3>
-				<p>Release, Pipe Dream’s arts &amp; entertainment section, covers all things cultural: from student productions, to Binghamton restaurants, to reviews of the latest movies and TV shows. Release writers often get behind-the-scenes access to concerts and events.</p>
-				<a href="mailto:release@bupipedream.com" class="section-email">release@bupipedream.com</a>
+			<div data-section="AE" class="section-description">
+				<h3 class="section-title">AE</h3>
+				<p>If you’ve ever read a great book, listened to a breathtaking song, or stopped to take a better look at a striking work of art, you’ve done what we do. As the Spectator’s department of Arts and Entertainment, a lot falls under our jurisdiction. From reviewing the delectable cuisine at events such as the Taste of Tribeca, covering red carpet premiers at the Tribeca Film Festival, and more, our scope is far from narrow. Though we give events in Tribeca priority, our reach spans all over the city. Ultimately, it is our duty to bring the city’s culture to the Stuyvesant community.</p>
+				<a href="http://stuyspec.com/join/ae-application/" class="section-email">Apply to A&amp;E</a>
 			</div>
+			<div data-section="Features" class="section-description">
+				<h3 class="section-title">Features</h3>
+				<p>If you’ve ever read a great book, listened to a breathtaking song, or stopped to take a better look at a striking work of art, you’ve done what we do. As the Spectator’s department of Arts and Entertainment, a lot falls under our jurisdiction. From reviewing the delectable cuisine at events such as the Taste of Tribeca, covering red carpet premiers at the Tribeca Film Festival, and more, our scope is far from narrow. Though we give events in Tribeca priority, our reach spans all over the city. Ultimately, it is our duty to bring the city’s culture to the Stuyvesant community.</p>
+				<a href="http://stuyspec.com/join/features-application/" class="section-email">Apply to Features</a>
+			</div>
+			<div data-section="Humor" class="section-description">
+				<h3 class="section-title">Humor</h3>
+				<p>If you’ve ever read a great book, listened to a breathtaking song, or stopped to take a better look at a striking work of art, you’ve done what we do. As the Spectator’s department of Arts and Entertainment, a lot falls under our jurisdiction. From reviewing the delectable cuisine at events such as the Taste of Tribeca, covering red carpet premiers at the Tribeca Film Festival, and more, our scope is far from narrow. Though we give events in Tribeca priority, our reach spans all over the city. Ultimately, it is our duty to bring the city’s culture to the Stuyvesant community.</p>
+				<a href="http://stuyspec.com/join/humor-application/" class="section-email">Apply to Humor</a>
+			</div>
+
 			<div data-section="copy" class="section-description">
 				<h3 class="section-title">Copy</h3>
 				<p>The copy department is responsible for making sure every Pipe Dream article and caption is grammatically correct and factually accurate. Copy editors hone their language skills as they clean up spelling mistakes, redundancies and ambiguities throughout the paper.</p>
-				<a href="mailto:copy@bupipedream.com" class="section-email">copy@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/copy-application/" class="section-email">Apply to Copy</a>
 			</div>
 
 			<!-- Tech Sections -->
 			<div data-section="web" class="section-description">
 				<h3 class="section-title">Web Development</h3>
 				<p>The web section is responsible for all things bupipedream.com. Developers get hands on experience using software, tools and service such as Git, GitHub, LESS and WordPress.</p>
-				<a href="mailto:developer@bupipedream.com" class="section-email">developer@bupipedream.com</a>
-			</div>
-			<div data-section="social" class="section-description">
-				<h3 class="section-title">Social Media</h3>
-				<p>The social media section manages Pipe Dream’s presence on Facebook and Twitter. Social media contributors get real-world experience working on the most active social media accounts on campus.</p>
-				<a href="mailto:social@bupipedream.com" class="section-email">social@bupipedream.com</a>
-			</div>
-			<div data-section="tech" class="section-description">
-				<h3 class="section-title">Technology</h3>
-				<p>The technology section lays the groundwork for an efficient working environment for the rest of the staff. Tackling challenges that range from modifying user accounts to wrestling with the printer, the tech department ensures Pipe Dream’s hardware and software is functioning properly.</p>
-				<a href="mailto:tech@bupipedream.com" class="section-email">tech@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/web-application/" class="section-email">Apply to Web</a>
 			</div>
 
+			<div data-section="video" class="section-description">
+				<h3 class="section-title">Media</h3>
+				<p>The technology section lays the groundwork for an efficient working environment for the rest of the staff. Tackling challenges that range from modifying user accounts to wrestling with the printer, the tech department ensures Pipe Dream’s hardware and software is functioning properly.</p>
+				<a href="http://www.stuyspec.com/" class="section-email">Apply to Media</a>
+			</div>
+			
+
 			<!-- Creative Sections -->
-			<div data-section="design" class="section-description">
-				<h3 class="section-title">Design</h3>
+			<div data-section="layout" class="section-description">
+				<h3 class="section-title">Layout</h3>
 				<p>The design team is responsible for compiling stories, headlines, photos, captions and credits into neat and creative layouts for each page of the issue. Designers collaborate with the design manager and section heads to effectively display information and graphics that appeal to a college audience.</p>
-				<a href="mailto:design@bupipedream.com" class="section-email">design@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/layout-application/" class="section-email">Apply to Layout</a>
 			</div>
 			<div data-section="photo" class="section-description">
 				<h3 class="section-title">Photo</h3>
 				<p>The photo section is responsible for photographing assignments when any news, sports or Release story needs accompanying art. Photographers get to work with a broad range of subject matter and can undertake independent assignments to be featured in special spreads or on Pipe Dream’s Facebook page.</p>
-				<a href="mailto:photo@bupipedream.com" class="section-email">photo@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/photography-application/" class="section-email">Apply to Photo</a>
 			</div>
-			<div data-section="fun" class="section-description">
-				<h3 class="section-title">Fun</h3>
+			<div data-section="art" class="section-description">
+				<h3 class="section-title">Art</h3>
 				<p>Fun page is a catch-all page that aims to cure students of their in-class boredom with comics, crosswords and Sudoku. Fun page contributors enjoy broad exposure and are encouraged to regularly submit original comics.</p>
-				<a href="mailto:fun@bupipedream.com" class="section-email">fun@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/art-application/" class="section-email">Apply to Art</a>
 			</div>
 
 			<!-- Business Sections -->
 			<div data-section="business" class="section-description">
 				<h3 class="section-title">Business</h3>
 				<p>The business department is in charge of all of Pipe Dream’s finances. As the managers of one of the Student Association’s largest student-run businesses, they handle things such as accounts receivable, bookkeeping and budgeting.</p>
-				<a href="mailto:business@bupipedream.com" class="section-email">business@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/business-application/" class="section-email">Apply to Business</a>
 			</div>
 			<div data-section="advertising" class="section-description">
 				<h3 class="section-title">Advertising</h3>
 				<p>As a branch of the business department, the advertising department makes cold calls to potential clients, processes existing account orders and acts as a liaison between clients and the rest of the paper.</p>
-				<a href="mailto:advertising@bupipedream.com" class="section-email">advertising@bupipedream.com</a>
+				<a href="http://stuyspec.com/join/business-application/" class="section-email">Apply to Business</a>
 			</div>
 
+
+
+
+
+
 			<ul id="section-writing" class="section-category">
-				<li class="active"><a href="mailto:news@bupipedream.com" title="Learn more about the news section" data-section="news">News</a></li>
-				<li><a href="mailto:sports@bupipedream.com" title="Learn more about the sports section" data-section="sports">Sports</a></li>
-				<li><a href="mailto:opinion@bupipedream.com" title="Learn more about the opinion section" data-section="opinion">Opinion</a></li>
-				<li><a href="mailto:release@bupipedream.com" title="Learn more about the release section" data-section="release">Release</a></li>
-				<li><a href="mailto:copy@bupipedream.com" title="Learn more about the copy section" data-section="copy">Copy</a></li>
+				<li class="active"><a href="http://stuyspec.com/join/news-application/" title="Learn more about the news section" data-section="news">News</a></li>
+				<li><a href="http://stuyspec.com/join/sports-application/" title="Learn more about the sports section" data-section="sports">Sports</a></li>
+				<li><a href="http://stuyspec.com/join/opinions-application/" title="Learn more about the opinions section" data-section="opinion">Opinions</a></li>
+				<li><a href="http://stuyspec.com/join/ae-application/" title="Learn more about the A&E section" data-section="ae">A&E</a></li>
+				<li><a href="http://stuyspec.com/join/features-application/" title="Learn more about the features section" data-section="features">Features</a></li>
+				<li><a href="http://stuyspec.com/join/humor-application/" title="Learn more about the humor section" data-section="humor">Humor</a></li>
+				<li><a href="http://stuyspec.com/join/copy-application/" title="Learn more about the copy section" data-section="copy">Copy</a></li>
 			</ul>
 			<ul id="section-tech" class="section-category">
-				<li><a href="mailto:developer@bupipedream.com" title="Learn more about the web section" data-section="web">Web</a></li>
-				<li><a href="mailto:social@bupipedream.com" title="Learn more about the social media section" data-section="social">Social Media</a></li>
-				<li><a href="mailto:tech@bupipedream.com" title="Learn more about the newsroom section" data-section="tech">Newsroom Tech</a></li>
+				<li><a href="http://stuyspec.com/join/web-application/" title="Learn more about the web section" data-section="web">Web</a></li>
+				<li><a href="http://stuyspec.com/join/web-application/" title="Learn more about the media section" data-section="video">Media</a></li>
 			</ul>
 			<ul id="section-creative" class="section-category">
-				<li><a href="mailto:design@bupipedream.com" title="Learn more about the design section" data-section="design">Design</a></li>
-				<li><a href="mailto:photo@bupipedream.com" title="Learn more about the photo section" data-section="photo">Photo</a></li>
-				<li><a href="mailto:fun@bupipedream.com" title="Learn more about the fun section" data-section="fun">Fun</a></li>
+				<li><a href="http://stuyspec.com/join/layout-application/" title="Learn more about the layout section" data-section="design">Layout</a></li>
+				<li><a href="http://stuyspec.com/join/photo-application/" title="Learn more about the photo section" data-section="photo">Photo</a></li>
+				<li><a href="http://stuyspec.com/join/art-application/" title="Learn more about the art section" data-section="art">Art</a></li>
 			</ul>
 			<ul id="section-business" class="section-category">
-				<li><a href="mailto:business@bupipedream.com" title="Learn more about the business section" data-section="business">Business</a></li>
-				<li><a href="mailto:advertising@bupipedream.com" title="Learn more about the advertising section" data-section="advertising">Advertising</a></li>
+				<li><a href="http://stuyspec.com/join/business-application/" title="Learn more about the business section" data-section="business">Business</a></li>
+				<li><a href="http://stuyspec.com/join/business-application/" title="Learn more about the advertising section" data-section="advertising">Advertising</a></li>
 			</ul>
 
 		</section>
 		<section class="page-section photo-grid">
-			<h2 class="page-section-title">Life at Pipe Dream</h2>
+			<h2 class="page-section-title">Life at The Spectator</h2>
 			<img src="<? bloginfo( 'template_url' ); ?>/img/pages/join/paige.jpg">
 			<img src="<? bloginfo( 'template_url' ); ?>/img/pages/join/paige.jpg">
 			<img src="<? bloginfo( 'template_url' ); ?>/img/pages/join/paige.jpg">
