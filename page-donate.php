d539dd06b54737c96c94a4c01028dd85f07b3ce6
Andrew Fischer <afischer15@mac.com>
2013-12-27 15:52:00 +0000

    Created donation page.

--- page-donate.php
+++ page-donate.php
@@ -13,7 +13,67 @@
 		</nav>
 		<h1 class="page-title"><?php the_title(); ?></h1>
 		<section class="post">
-			<?php the_content(); ?>
+		<?php the_content(); ?>
+
+		<br>
+
+
+		<!--Bitcoin Donations-->
+		<script src="http://coinwidget.com/widget/coin.js"></script>
+		<script>
+		CoinWidgetCom.go({
+			wallet_address: "18KULfsgimwbQ5naTeKJgUY83nHrhNKsGW"
+			, currency: "bitcoin"
+			, counter: "count"
+			, alignment: "bl"
+			, qrcode: true
+			, auto_show: false
+			, lbl_button: "Donate Bitcoins"
+			, lbl_address: "The Spectator's Bitcoin Address:"
+			, lbl_count: "Donations"
+			, lbl_amount: "BTC Donated"
+		});
+		</script>
+
+
+		<!--Litecoin Donations-->
+		<script src="http://coinwidget.com/widget/coin.js"></script>
+		<script>
+		CoinWidgetCom.go({
+			wallet_address: "LbJ8wcKHtCZJHc1CynuLVz8s2iaVEwCCaR"
+			, currency: "litecoin"
+			, counter: "count"
+			, alignment: "bl"
+			, qrcode: true
+			, auto_show: false
+			, lbl_button: "Donate Litecoins"
+			, lbl_address: "The Spectator's Litecoin Address:"
+			, lbl_count: "Donations"			
+			, lbl_amount: "LTC Donated"
+		});
+		</script>
+
+
+		<!--Dogecoin Donations-->
+		<script src="http://cdn.bitmindo.com/dogecoin.min.js"></script>
+		<script>
+		CoinWidgetCom.go({
+			wallet_address: "D8qsvWFrawNz3omfaFMM4mXC4oq2GZRivf"
+   			, currency: "dogecoin"
+    			, counter: "count"
+    			, alignment: "bl"
+    			, qrcode: true
+			, auto_show: false
+			, lbl_button: "Donate Dogecoins"
+		 	, lbl_address: "The Spectator's Dogecoin Address:"
+	                , lbl_amount: "DOGE Donated"
+		});
+		</script>
+
+
+		<br>
+
+
 		</section>
 	</div>
 	<?php get_footer(); ?>
